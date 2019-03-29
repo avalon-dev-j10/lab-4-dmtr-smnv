2c13826d88d52c1d709836fe442bed2c766a57c6
Semenov_D <8933626@mail.ru>
2019-03-29 14:26:48 +0300

    Лабораторная работа #4 (финальная версия)

--- src/ru/avalon/java/dev/j10/labs/Compare.java
+++ src/ru/avalon/java/dev/j10/labs/Compare.java
@@ -2,16 +2,10 @@ package ru.avalon.java.dev.j10.labs;
 
 import java.util.Comparator;
 
-public class Compare implements Comparator {
+public class Compare implements Comparator<String> {
 
-    @Override
-    public int compare(Object o1, Object o2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Comparator reversed() {
-        return Comparator.super.reversed(); //To change body of generated methods, choose Tools | Templates.
+    public int compare(String string1, String string2) {
+        return string1.compareTo(string2);
     }
 
 }
